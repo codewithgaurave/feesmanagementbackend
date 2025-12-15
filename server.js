7d57d1a6cc9b407f682d2b609cda13c9241b03e7
digyGaurav <gaurav.gupta@digy4.com>
2025-12-15 14:49:29 +0530

    done

--- server.js
+++ server.js
@@ -16,8 +16,15 @@ const app = express();
 connectDB();
 
 app.use(cors({
-  origin: ['http://localhost:5000', 'http://localhost:5173', 'http://localhost:5174'],
-  credentials: true
+  origin: [
+    "http://localhost:5000",
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://feesmanagementfrontend.onrender.com"
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 // Custom JSON parser that handles malformed JSON
